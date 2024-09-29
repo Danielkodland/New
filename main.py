4413d7ba6268dc7aa113bfebe39a883bc54cbfdc
Danielkodland <elproxdsoyunpro@gmail.com>
2024-09-28 19:06:22 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+# Import
+from flask import Flask, render_template,request, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+
+
+app = Flask(__name__)
+
+# Página de contenidos en ejecución
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+# Habilidades dinámicas
+@app.route('/', methods=['POST'])
+def process_form():
+    button_python = request.form.get('button_python')
+    return render_template('index.html', button_python=button_python)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+db = SQLAlchemy(app)
+
+# Creación de una tabla
+class Card(db.Model):
+    # Creación de columnas
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    subtitle = db.Column(db.String(300), nullable=False)
+    text = db.Column(db.Text, nullable=False)
